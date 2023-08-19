5fc6b8d54a7a6c0db1a7d843dea809cfeb7bf637
Talha <zubaer.talha20@gmail.comm>
2023-08-19 16:16:18 +0600

    No major changes

--- script.js
+++ script.js
@@ -144,6 +144,7 @@ btnLogin.addEventListener('click', function(e){
       labelWelcome.textContent = `Welcome back ${curAcc.owner.split(' ')[0]}!`
       containerApp.style.opacity = 100
   }
+  inputLoginUsername.value = inputLoginPin.value = ''
 })
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
